e684115ec503c9f996ed954329dc5acde986f4a5
Jamie-Sevior <43117390+Jamie-Sevior@users.noreply.github.com>
2021-09-16 09:44:36 +0100

    Add files via upload

---
+++ G3/G3/Startup.cs
@@ -0,0 +1,66 @@
+using G3.DAL;
+using G3.Process;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace G3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllersWithViews();
+
+            var context = new Context(new DbContextOptionsBuilder<Context>().UseInMemoryDatabase(databaseName: "G3").Options);
+            context.Database.EnsureCreated();
+
+            services.AddSingleton<Context>(context);
+            services.AddScoped<INotificationProcessor, NotificationProcessor>();
+            services.AddTransient<IWatchVehicleProcessor, WatchVehicleProcessor>();
+            services.AddTransient<IPriceProcessor, PriceProcessor>();
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller}/{action=Index}/{id?}");
+            });
+
+        }
+    }
+}
